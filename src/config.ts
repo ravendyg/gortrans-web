04bafa8b6fdb40f85c1982196dd6530c24a7651e
Yaroslav Dashkevich <y.dashkevich@amber.software>
2016-10-19 11:10:31 +0700

    Change config to work both with http and https

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@
 const config =
 {
   NUMBER_OF_BUSES_LIMIT: 5,
-  URL: 'http://maps.nskgortrans.info',
+  URL: '//maps.nskgortrans.info',
   GET_LIST_OF_ROUTES: '/list-of-routes',
   GET_LIST_OF_ROUTE_CODES: '/list-of-route-codes'
 };
