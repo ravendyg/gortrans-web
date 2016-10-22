3e15be3400be7c5ea852fa9188c6a65fb0be81e1
Yaroslav <jaroslav.dashkevich@gmail.com>
2016-10-22 12:17:26 +0700

    Fix removing old route and markers if vehicle is shifted by a new one when a limit is reached

--- src/services/user-actions.ts
+++ src/services/user-actions.ts
@@ -1,6 +1,7 @@
 /// <reference path="../typings/index.d.ts" />
 'use strict';
 
+import { config } from '../config';
 import { Socket } from './data-provider';
 import { ActionCreators } from './action-creators';
 import { Store } from './store';
@@ -9,8 +10,17 @@ import {Map} from './map';
 
 export function addBus(bus: VehicleMeta)
 {
-  Store.dispatch( ActionCreators.addBusToList(bus) );
-  Socket.addBusListener(bus.code);
+  var currentBusList: VehicleMeta [] = (Store.getState() as ReduxState).busList;
+  if ( !currentBusList['find'](e => e.code === bus.code) )
+  { // not yet there
+    Store.dispatch( ActionCreators.addBusToList(bus) );
+    Socket.addBusListener(bus.code);
+    if ( currentBusList.length >= config.NUMBER_OF_BUSES_LIMIT )
+    { // limit number of routes on the map at the same time
+      Socket.removeBusListener( currentBusList[0].code );
+      Map.removeVehicle( currentBusList[0].code );
+    }
+  }
 };
 
 export function removeBus(bus: VehicleMeta)
